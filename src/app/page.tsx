fef74a733bf90661c201f8074e6a4aff9d649e05
Sobirov3636 <sobirov3636@gmail.com>
2024-01-12 22:20:23 +0900

    changes saved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import homeImg from "/public/home.jpg";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='relative z-10'>
+      Home page
+      <div className='absolute -z-10 inset-0'>
+        <Image src={homeImg} alt='car factory' fill style={{ objectFit: "cover" }} />
+      </div>
+    </div>
+  );
 }
